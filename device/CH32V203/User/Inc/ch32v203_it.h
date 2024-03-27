5f75b7bb8f8d12aa2043abad0725b6b1b687e346
徐瑞骏 <1687701765@qq.com>
2024-03-27 17:05:41 +0800

    fix: 把CH32V203/User符号链接替换成实际目录，修复同时引用CH32V103和CH32V203头文件导致重定义编译错误

---
+++ device/CH32V203/User/Inc/ch32v203_it.h
@@ -0,0 +1,10 @@
+/*******************************************************************************
+ * Interrupt handlers for CH32V203
+ * Copyright (C) 2023-2024 Xu Ruijun
+ *
+ * License under BSD 3-clause, see LICENSE.BSD3 file
+ ******************************************************************************/
+#ifndef CH32V203_IT_H
+#define CH32V203_IT_H
+
+#endif
