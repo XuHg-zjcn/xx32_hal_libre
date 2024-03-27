c26f6e8176ab8cdfff2821d5fb59b3948f2f786b
徐瑞骏 <1687701765@qq.com>
2024-03-27 22:58:42 +0800

    fix: 修复C++编译错误

--- device/CH32V203/Inc/ch32v203_delay.h
+++ device/CH32V203/Inc/ch32v203_delay.h
@@ -3,8 +3,16 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C"{
+#endif
+
 void Delay_Init();
 void Delay_us(uint32_t us);
 void Delay_ms(uint32_t ms);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
